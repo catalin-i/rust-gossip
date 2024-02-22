76c6bf2a712906d4e600fdc7b933900ad4f3f082
Catalin Iosif <catalin.iosif.me@gmail.com>
2024-02-22 21:13:31 +0200

    implement unique-id

---
+++ scripts/unique-id.sh
@@ -0,0 +1,1 @@
+$MAELSTROM_LOC/maelstrom test -w unique-ids --bin ../target/debug/unique-id --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
---
+++ unique-id/src/main.rs
@@ -0,0 +1,51 @@
+use std::str::FromStr;
+
+use maelstrom_rs::{
+    actor::Actor,
+    error::Error,
+    message::{Request, Response},
+    runtime::Runtime,
+};
+use serde_json::{Map, Value};
+
+fn main() {
+    let node = UuidActor { node_id: None };
+    let mut runtime = Runtime::new(Box::new(node));
+    runtime.start();
+}
+
+struct UuidActor {
+    node_id: Option<String>,
+}
+
+impl Actor for UuidActor {
+    fn init(
+        &mut self,
+        node_id: &str,
+        node_ids: Vec<String>,
+    ) -> Result<(), maelstrom_rs::error::Error> {
+        self.node_id = Some(String::from(node_id));
+        eprintln!("node {} initialized", node_id);
+        Ok(())
+    }
+
+    fn receive(
+        &mut self,
+        request: &maelstrom_rs::message::Request,
+    ) -> Result<Vec<maelstrom_rs::message::Response>, maelstrom_rs::error::Error> {
+        match request.message_type.as_str() {
+            "generate" => self.handle_generate(request),
+            _ => unimplemented!("not implemented"),
+        }
+    }
+}
+
+impl UuidActor {
+    pub(crate) fn handle_generate(&self, request: &Request) -> Result<Vec<Response>, Error> {
+        let mut body = Map::new();
+        let uuid = uuid::Uuid::new_v4().to_string();
+        eprintln!("generated id: {}", uuid);
+        body.insert("id".to_string(), Value::String(uuid));
+        Ok(vec![Response::new_from_request(request, body)])
+    }
+}
